245fdf5d5951d3a2909ce4ac6420329402528f1d
Wilson Mbiuki <wilson.mbiuki@student.moringaschool.com>
2026-05-13 09:26:22 +0300

    useTimer.js code upload

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -1,6 +1,23 @@
 // useTimer — Member 3 working on this
-function useTimer() {
-  return null
+import { useState, useEffect } from "react";
+
+function useTimer(seconds, onTimeUp) {
+  const [timeLeft, setTimeLeft] = useState(seconds);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      onTimeUp();
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval); // cleanup!
+  }, [timeLeft]);
+
+  return timeLeft;
 }
 
-export default useTimer
+export default useTimer;
