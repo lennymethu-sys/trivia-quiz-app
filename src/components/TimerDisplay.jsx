8be046159117bf898f02bf56dc8e37f76c197276
Wilson Mbiuki <wilson.mbiuki@student.moringaschool.com>
2026-05-13 09:27:07 +0300

    TimerDisplay.jsx

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,5 +1,13 @@
-function TimerDisplay() {
-  return <div>TimerDisplay — Member 3 working on this</div>
+import useTimer from "../hooks/useTimer";
+
+function TimerDisplay({ seconds, onTimeUp }) {
+  const timeLeft = useTimer(seconds, onTimeUp);
+
+  return (
+    <div>
+      <p>Time left: {timeLeft}s</p>
+    </div>
+  );
 }
 
-export default TimerDisplay
+export default TimerDisplay;
