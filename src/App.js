86bf42e90f75e132a6013c54062279006ab1ae21
Wilson Mbiuki <wilson.mbiuki@student.moringaschool.com>
2026-05-14 23:43:33 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TimerDisplay from './components/TimerDisplay'; 
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header><TimerDisplay seconds={15} onTimeUp={() => console.log("Time's up!")} />
+
     </div>
   );
 }
