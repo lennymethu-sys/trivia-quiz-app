3b9cc0f4c71608b5c0137602b45f45ccaf73139a
Yrimu-Wan-code <patiencegachie@gmail.com>
2026-05-14 11:54:27 +0300

    feat: add functionality to Answer Review component

--- src/components/AnswerReview.jsx
+++ src/components/AnswerReview.jsx
@@ -1,5 +1,34 @@
-function AnswerReview() {
-  return <div>AnswerReview — Member 4 working on this</div>
+function AnswerReview(props) {
+  
+  //destructuring the props
+  const isCorrect = props.isCorrect;
+  const correctAnswer = props.correctAnswer;
+  const onNext = props.onNext;
+
+  return(
+<div>
+  <div style={{   }}></div>
+
+<div>
+{/* When user gets the answer right or wrong */}
+{isCorrect ? (
+  <div>
+    <h2>Correct! Good job!</h2>
+  </div>
+) : (
+  <div>
+    <h2>Incorrect! The correct answer is: {correctAnswer}</h2>
+  </div>
+)}
+
+{/* Next button to move to the next question */}
+  <div>
+    <button onClick={onNext}>Next Question</button>
+  </div>
+</div>
+</div>
+  );
 }
+
 
 export default AnswerReview
