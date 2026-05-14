cd2481b6918238bbf803eb57420fc652caaf547a
Yrimu-Wan-code <patiencegachie@gmail.com>
2026-05-14 12:14:25 +0300

    fix: pass the percentageScore to the progress bar

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProgressBar from './ProgressBar';
 
 function ScoreBoard (props) {
 
@@ -16,6 +17,7 @@ function ScoreBoard (props) {
       <div>
         {percentageScore}% You scored {score} out of {totalQuestions}!
       </div>
+      <ProgressBar percentage={percentageScore} />
       <div>
       <button onClick={onRestart}>Restart Quiz</button>
       </div>
