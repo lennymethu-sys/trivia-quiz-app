584822519c9e739ccc2f9b939dc0810c8467dc4e
lennymethu-sys <lenny.methu@student.moringaschool.com>
2026-05-15 10:50:22 +0300

    fix: add useTimer import to TimerDisplay

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,9 +1,14 @@
-function TimerDisplay({ seconds, onTimeUp }) {
-  const { timeLeft } = useTimer(seconds, onTimeUp); // ← destructure now
+import useTimer from "../hooks/useTimer"
 
+function TimerDisplay({ seconds, onTimeUp }) {
+  const { timeLeft } = useTimer(seconds, onTimeUp)
   return (
-    <div>
+    <div className="timer">
       <p>Time left: {timeLeft}s</p>
     </div>
-  );
+  )
 }
+
+export default TimerDisplay
+
+
